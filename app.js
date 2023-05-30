fe872d480536f00abc6daf7629a6daf732ea0225
PerniDevs <pernigotti.developments@gmail.com>
2023-05-30 18:49:03 +0200

    DB encryption

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //jshint esversion:6
-
 // Modules declaration
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -7,11 +6,11 @@ const ejs = require("ejs");
 const _ = require("lodash")
 const mongoose = require("mongoose")
 const app = express();
+var encrypt = require('mongoose-encryption');
 
 // ENV variables
 require("dotenv").config();
 const PORT = process.env.PORT
-
 
 // APP set and use
 app.set('view engine', 'ejs');
@@ -20,10 +19,12 @@ app.use(express.static("public"));
 
 // DB construction
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true})
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+const secret = "Thisisourlittlescret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']})
 const User = new mongoose.model("User", userSchema)
 
 //APP RUNNING
@@ -54,18 +55,25 @@ app.post("/register", function(req, res){
     })
 })
 
-app.post("/login", function(req,res){
-    const username = req.body.username
-    const password = req.body.password
-    User.findOne({email: username, password: password})
-    .then(function(foundUser){
-        if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets")
+app.post("/login", function(req, res){
+    const loginusername = req.body.username;
+    const loginpassword = req.body.password;
+    User.find({email: loginusername, password: loginpassword}).exec()
+        .then(function(foundUser) {
+            if (foundUser) {
+                // User found, handle authentication success
+                res.render("Secrets");
+            } else {
+                // User not found or authentication failed
+                console.log("User not found or authentication failed");
             }
-        }
-    })
-})
+        })
+        .catch(function(err) {
+            // Error occurred during database query
+            console.log(err);
+        });
+});
+
 
 //LISTEN
 app.listen(PORT, function () {
